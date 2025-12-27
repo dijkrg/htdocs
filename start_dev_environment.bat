1c09ef4969f8f1d745bbbe1fc12afe5684af65a8
Thuiskantoor <info@ancorna.nl>
2025-12-27 17:20:17 +0100

    Updated index.php and start_dev_environment.bat for deployment

--- start_dev_environment.bat
+++ start_dev_environment.bat
@@ -3,17 +3,21 @@
 REM Set the path to your XAMPP installation directory
 set XAMPP_PATH="C:\xampp"
 
+REM Ensure Apache serves the correct site
+set APACHE_CONF=%XAMPP_PATH%\apache\conf\httpd.conf
+
+REM Update DocumentRoot to point to the office folder
+powershell -Command "(Get-Content %APACHE_CONF%) -replace 'DocumentRoot \".*?\"', 'DocumentRoot \"C:/Users/rvand/OneDrive/Bureaublad/htdocs/office\"' | Set-Content %APACHE_CONF%"
+powershell -Command "(Get-Content %APACHE_CONF%) -replace '<Directory \".*?\">', '<Directory \"C:/Users/rvand/OneDrive/Bureaublad/htdocs/office\">' | Set-Content %APACHE_CONF%"
+
 REM Start Apache server
 start "" %XAMPP_PATH%\apache_start.bat
-
-REM Start MySQL server
-start "" %XAMPP_PATH%\mysql_start.bat
 
 REM Simuleer de online omgeving door de hostnaam te forceren
 set HTTP_HOST=office.abcbrandbeveiliging.nl
 
 REM Open de applicatie in de standaardbrowser
-start "" "http://localhost"
+start "" "http://localhost/index.php"
 
 REM Wacht op gebruikersinvoer om het script te sluiten
 pause
